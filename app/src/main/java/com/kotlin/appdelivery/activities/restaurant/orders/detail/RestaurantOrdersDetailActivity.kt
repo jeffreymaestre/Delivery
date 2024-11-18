205088993a53b478b2acc5a751c483f5d974e42a
Jeffrey Maestre <36870162+jeffreymaestre@users.noreply.github.com>
2024-11-17 23:15:28 -0500

    Add: display orders in the restaurant role

---
+++ app/src/main/java/com/kotlin/appdelivery/activities/restaurant/orders/detail/RestaurantOrdersDetailActivity.kt
@@ -0,0 +1,82 @@
+package com.kotlin.appdelivery.activities.restaurant.orders.detail
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.kotlin.appdelivery.R
+import com.kotlin.appdelivery.adapters.OrderProductsAdapter
+import com.kotlin.appdelivery.adapters.OrdersClientAdapter
+import com.kotlin.appdelivery.models.Order
+
+class RestaurantOrdersDetailActivity : AppCompatActivity() {
+
+    val TAG = "ClientOrderDetail"
+    var order: Order? = null
+    val gson = Gson()
+
+    var toolbar: Toolbar? = null
+
+    var texViewClient: TextView? = null
+    var texViewAddress: TextView? = null
+    var texViewData: TextView? = null
+    var texViewTotal: TextView? = null
+    var texViewStatus: TextView? = null
+    var recyclerViewProducts: RecyclerView? = null
+
+    var adapter: OrderProductsAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_restaurant_orders_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        order = gson.fromJson(intent.getStringExtra("order"), Order::class.java)
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitleTextColor(ContextCompat.getColor(this, R.color.black))
+        toolbar?.title = "Order #${order?.id}"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        texViewClient = findViewById(R.id.textview_client)
+        texViewAddress = findViewById(R.id.textview_address)
+        texViewData = findViewById(R.id.textview_date)
+        texViewTotal = findViewById(R.id.textview_total)
+        texViewStatus = findViewById(R.id.textview_status)
+
+        recyclerViewProducts = findViewById(R.id.recyclerview_products)
+        recyclerViewProducts?.layoutManager = LinearLayoutManager(this)
+
+        adapter = OrderProductsAdapter(this, order?.products!!)
+        recyclerViewProducts?.adapter = adapter
+
+        texViewClient?.text = "${order?.client?.name} ${order?.client?.lastname}"
+        texViewAddress?.text = order?.address?.address
+        texViewData?.text = "${order?.timestamp}"
+        texViewStatus?.text = order?.status
+
+        Log.d(TAG, "Orden: ${order.toString()}")
+
+        getTotal()
+    }
+
+    private fun getTotal(){
+        var total = 0.0
+        for (p in order?.products!!){
+            total = total + (p.price * p.quantity!!)
+        }
+        texViewTotal?.text = "${total}$"
+    }
+}
---
+++ app/src/main/java/com/kotlin/appdelivery/fragments/restaurant/RestaurantOrdersStatusFragment.kt
@@ -0,0 +1,86 @@
+package com.kotlin.appdelivery.fragments.restaurant
+
+import com.kotlin.appdelivery.adapters.OrdersRestaurantAdapter
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.kotlin.appdelivery.R
+import com.kotlin.appdelivery.adapters.OrdersClientAdapter
+import com.kotlin.appdelivery.models.Order
+import com.kotlin.appdelivery.models.User
+import com.kotlin.appdelivery.providers.OrdersProvider
+import com.kotlin.appdelivery.utils.SharePref
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.ArrayList
+
+class RestaurantOrdersStatusFragment : Fragment() {
+
+    var myView: View? = null
+    var ordersProvider: OrdersProvider? = null
+    var user: User? = null
+    var sharePref: SharePref? = null
+
+    var recyclerViewOrders: RecyclerView? = null
+    var adapter: OrdersRestaurantAdapter? = null
+
+    var status = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        myView =  inflater.inflate(R.layout.fragment_restaurant_orders_status, container, false)
+
+        sharePref = SharePref(requireActivity())
+        status = arguments?.getString("status")!!
+
+        getUserFromSession()
+        ordersProvider = OrdersProvider(user?.sessionToken!!)
+
+        recyclerViewOrders = myView?.findViewById(R.id.recyclerview_orders)
+        recyclerViewOrders?.layoutManager = LinearLayoutManager(requireContext())
+
+        getOrders()
+
+        return myView
+    }
+
+    private fun getOrders(){
+        ordersProvider?.getOrdersByStatus(status)?.enqueue(object: Callback<ArrayList<Order>>{
+            override fun onResponse(
+                call: Call<ArrayList<Order>>,
+                response: Response<ArrayList<Order>>
+            ) {
+                if (response.body() != null){
+                    val orders = response.body()
+                    adapter = OrdersRestaurantAdapter(requireActivity(), orders!!)
+                    recyclerViewOrders?.adapter = adapter
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<Order>>, t: Throwable) {
+                Toast.makeText(requireActivity(), "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+    private fun getUserFromSession(){
+        val gson = Gson()
+
+        if (!sharePref?.getData("user").isNullOrBlank()){
+            // VALIDO SI EL USUARIO EXISTE EN SESION
+            user = gson.fromJson(sharePref?.getData("user"), User::class.java)
+        }
+    }
+
+}
