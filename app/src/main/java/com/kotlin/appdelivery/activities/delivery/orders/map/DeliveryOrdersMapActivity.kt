a73ddc9662dadfdf141104b81223b6253b0a6df9
Jeffrey Maestre <36870162+jeffreymaestre@users.noreply.github.com>
2024-11-28 23:34:12 -0500

    Add: functionality to display origin and destination location once NOT in real time

---
+++ app/src/main/java/com/kotlin/appdelivery/activities/delivery/orders/map/DeliveryOrdersMapActivity.kt
@@ -0,0 +1,269 @@
+package com.kotlin.appdelivery.activities.delivery.orders.map
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
+import com.kotlin.appdelivery.R
+import com.kotlin.appdelivery.models.Order
+import java.net.URI.create
+
+class DeliveryOrdersMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    val TAG = "DeliveryOrdersMap"
+    var googleMap: GoogleMap? = null
+
+    val PERMISSION_ID = 97
+    var fusedLocationClient: FusedLocationProviderClient? = null
+
+    var buttonAcept: Button? = null
+
+    var city = ""
+    var country = ""
+    var address = ""
+    var addresLatLong: LatLng? = null
+
+    var markerDelivery: Marker? = null
+    var markerAddress: Marker? = null
+    var myLocationLatLong: LatLng? = null
+
+    var order: Order? = null
+    val gson = Gson()
+
+    private val locationCallback = object: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            var lastLocation = locationResult.lastLocation
+            Log.d("LOCALIZACION",  "Callback: ${lastLocation}")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_delivery_orders_map)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        order = gson?.fromJson(intent.getStringExtra("order"), Order::class.java)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        buttonAcept = findViewById(R.id.btn_acept)
+
+        getLastLocation()
+
+        buttonAcept?.setOnClickListener{ goToCreateAddress() }
+    }
+
+    private fun goToCreateAddress(){
+        val i = Intent()
+        i.putExtra("city", city)
+        i.putExtra("address", address)
+        i.putExtra("country", country)
+        i.putExtra("lat", addresLatLong?.latitude)
+        i.putExtra("lng", addresLatLong?.longitude)
+        setResult(RESULT_OK, i)
+        finish() // Volver hacia atras
+    }
+
+    private fun addDeliveryMarker(){
+        markerDelivery = googleMap?.addMarker(
+            MarkerOptions()
+                .position(myLocationLatLong)
+                .title("Mi posicion")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.delivery))
+        )
+    }
+
+    private fun addAddressMarker(){
+       val addressLocation = LatLng(order?.address?.lat!!, order?.address?.lng!!)
+        markerDelivery = googleMap?.addMarker(
+            MarkerOptions()
+                .position(addressLocation)
+                .title("Entregar aquí")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.home))
+        )
+    }
+
+    /*private fun addAddressMarker() {
+        // Verificar si 'order' y 'order.address' no son null
+        val address = order?.address
+        if (address != null) {
+            // Imprimir latitud y longitud en los logs
+            Log.d("AddAddressMarkerDebug", "Latitud: ${address.lat}")
+            Log.d("AddAddressMarkerDebug", "Longitud: ${address.lng}")
+
+            // Crear el LatLng con los valores de latitud y longitud
+            val addressLocation = LatLng(address.lat, address.lng)
+            markerDelivery = googleMap?.addMarker(
+                MarkerOptions()
+                    .position(addressLocation)
+                    .title("Entregar aquí")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.home))
+            )
+        } else {
+            // Manejar el caso cuando 'address' sea null
+            Log.d("AddAddressMarkerDebug", "ORDER: ${order}")
+            Log.d("AddAddressMarkerDebug", "Latitud: ${address?.lat}")
+            Log.d("AddAddressMarkerDebug", "Longitud: ${address?.lng}")
+            Log.e("AddAddressMarkerDebug", "La dirección es null en el objeto 'order'")
+        }
+    }*/
+
+
+
+    private fun getLastLocation() {
+        if (checkPermision()) {
+            if (isLocationEnabled()) {
+                // Verificamos nuevamente los permisos antes de acceder a la ubicación
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Solicitamos los permisos si no están otorgados
+                    requestPermision()
+                    return
+                }
+
+                // Obtener la última ubicación si ya tenemos los permisos
+                fusedLocationClient?.lastLocation?.addOnCompleteListener { task ->
+                    val location = task.result
+                    myLocationLatLong = LatLng(location.latitude, location.longitude)
+
+                    addDeliveryMarker()
+                    addAddressMarker()
+
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        googleMap?.moveCamera(
+                            CameraUpdateFactory.newCameraPosition(
+                                CameraPosition.builder().target(
+                                    LatLng(location.latitude, location.longitude)
+                                ).zoom(15f).build()
+                            )
+                        )
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Habilita la localización", Toast.LENGTH_SHORT).show()
+                val i = Intent(Settings.ACTION_LOCALE_SETTINGS)
+                startActivity(i)
+            }
+        } else {
+            requestPermision()
+        }
+    }
+
+
+    private fun requestNewLocationData() {
+        val locationRequest = LocationRequest.create().apply {
+            interval = 100
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient?.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermision(): Boolean{
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun requestPermision(){
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // El permiso fue concedido
+                getLastLocation()
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap?.uiSettings?.isZoomControlsEnabled = true
+    }
+}
